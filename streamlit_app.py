9012af964e20d3cfcf4951a655fcfd153f294593
Arni1234-cireng <arniflorentina@gmail.com>
2025-08-03 13:44:40 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,6 @@ import streamlit as st
 
 st.title("_menghitung_ lingkaran] :rocket:")
 r = st.number_input("masukan jari-jari (cm):", 0)
-Ifst.button("hitung Lingkaran", type="primary"):
+If st.button("hitung Lingkaran", type="primary"):
 L=Math.pi*(r**2)
 st.success(f"lingkaran adalah (L:.2f)")
